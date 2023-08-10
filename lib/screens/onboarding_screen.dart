cd5007bb4ba4fc502d983349ae5ba7fa28cac9ce
Divyansh Mehta <72566559+Divyansh-Mehta@users.noreply.github.com>
2023-08-10 18:54:20 +0530

    changes system bar style

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import './size_config.dart';
 import '../models/onboarding_contents.dart';
 import "./tabs_screen.dart";
@@ -50,173 +52,183 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     double width = SizeConfig.screenW!;
     double height = SizeConfig.screenH!;
 
-    return Scaffold(
-      backgroundColor: Color(0xffDCF6E6),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              flex: 3,
-              child: PageView.builder(
-                physics: const BouncingScrollPhysics(),
-                controller: _controller,
-                onPageChanged: (value) => setState(() => _currentPage = value),
-                itemCount: contents.length,
-                itemBuilder: (context, i) {
-                  return Padding(
-                    padding: const EdgeInsets.all(40.0),
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          height: (height >= 840) ? 60 : 30,
-                        ),
-                        Image.asset(
-                          contents[i].image,
-                          height: SizeConfig.blockV! * 35,
-                        ),
-                        SizedBox(
-                          height: (height >= 840) ? 60 : 30,
-                        ),
-                        Text(
-                          contents[i].desc,
-                          style: TextStyle(
-                            fontFamily: "Mulish",
-                            fontWeight: FontWeight.w300,
-                            fontSize: (width <= 550) ? 17 : 25,
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      // value: SystemUiOverlayStyle.light,
+      value: const SystemUiOverlayStyle(
+        statusBarColor: Color(0xffDCF6E6),
+        statusBarIconBrightness: Brightness.dark,
+        statusBarBrightness: Brightness.light,
+      ),
+      child: Scaffold(
+        backgroundColor: const Color(0xffDCF6E6),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                flex: 3,
+                child: PageView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  controller: _controller,
+                  onPageChanged: (value) =>
+                      setState(() => _currentPage = value),
+                  itemCount: contents.length,
+                  itemBuilder: (context, i) {
+                    return Padding(
+                      padding: const EdgeInsets.all(40.0),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: (height >= 840) ? 60 : 30,
                           ),
-                          textAlign: TextAlign.center,
-                        )
-                      ],
-                    ),
-                  );
-                },
+                          Image.asset(
+                            contents[i].image,
+                            height: SizeConfig.blockV! * 35,
+                          ),
+                          SizedBox(
+                            height: (height >= 840) ? 60 : 30,
+                          ),
+                          Text(
+                            contents[i].desc,
+                            style: TextStyle(
+                              fontFamily: "Mulish",
+                              fontWeight: FontWeight.w300,
+                              fontSize: (width <= 550) ? 17 : 25,
+                            ),
+                            textAlign: TextAlign.center,
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ),
               ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: List.generate(
-                      contents.length,
-                      (int index) => _buildDots(
-                        index: index,
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                        contents.length,
+                        (int index) => _buildDots(
+                          index: index,
+                        ),
                       ),
                     ),
-                  ),
-                  _currentPage + 1 == contents.length
-                      ? Padding(
-                          padding: const EdgeInsets.all(30),
-                          child: Column(
-                            children: [
-                              ElevatedButton(
-                                onPressed: () {
-                                  Navigator.of(context).pushReplacementNamed(
-                                      TabsScreen.routeName);
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor:
-                                      const Color.fromRGBO(50, 75, 80, 1),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(50),
+                    _currentPage + 1 == contents.length
+                        ? Padding(
+                            padding: const EdgeInsets.all(30),
+                            child: Column(
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pushReplacementNamed(
+                                        TabsScreen.routeName);
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor:
+                                        const Color.fromRGBO(50, 75, 80, 1),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(50),
+                                    ),
+                                    padding: (width <= 550)
+                                        ? const EdgeInsets.symmetric(
+                                            horizontal: 100, vertical: 20)
+                                        : EdgeInsets.symmetric(
+                                            horizontal: width * 0.2,
+                                            vertical: 25),
+                                    textStyle: TextStyle(
+                                        fontSize: (width <= 550) ? 13 : 17),
                                   ),
-                                  padding: (width <= 550)
-                                      ? const EdgeInsets.symmetric(
-                                          horizontal: 100, vertical: 20)
-                                      : EdgeInsets.symmetric(
-                                          horizontal: width * 0.2,
-                                          vertical: 25),
-                                  textStyle: TextStyle(
-                                      fontSize: (width <= 550) ? 13 : 17),
-                                ),
-                                child: const Text(
-                                  "Get Started",
-                                  style: TextStyle(
-                                    color: Color(0xffDCF6E6),
-                                  ),
-                                ),
-                              ),
-                              TextButton(
-                                onPressed: () {
-                                  _controller.jumpToPage(2);
-                                },
-                                style: TextButton.styleFrom(
-                                  elevation: 0,
-                                  textStyle: TextStyle(
-                                    fontWeight: FontWeight.w600,
-                                    fontSize: (width <= 550) ? 13 : 17,
+                                  child: const Text(
+                                    "Get Started",
+                                    style: TextStyle(
+                                      color: Color(0xffDCF6E6),
+                                    ),
                                   ),
                                 ),
-                                child: const Text(
-                                  "Already Have An Account? Log In",
-                                  style: TextStyle(
-                                    color: Color.fromRGBO(50, 75, 80, 1),
+                                TextButton(
+                                  onPressed: () {
+                                    _controller.jumpToPage(2);
+                                  },
+                                  style: TextButton.styleFrom(
+                                    elevation: 0,
+                                    textStyle: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: (width <= 550) ? 13 : 17,
+                                    ),
+                                  ),
+                                  child: const Text(
+                                    "Already Have An Account? Log In",
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(50, 75, 80, 1),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
+                          )
+                        : Padding(
+                            padding: const EdgeInsets.all(30),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                TextButton(
+                                  onPressed: () {
+                                    _controller.jumpToPage(2);
+                                  },
+                                  style: TextButton.styleFrom(
+                                    elevation: 0,
+                                    textStyle: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: (width <= 550) ? 13 : 17,
+                                    ),
+                                  ),
+                                  child: const Text(
+                                    "SKIP",
+                                    style: TextStyle(
+                                        color: Color.fromRGBO(50, 75, 80, 1)),
+                                  ),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    _controller.nextPage(
+                                      duration:
+                                          const Duration(milliseconds: 200),
+                                      curve: Curves.easeIn,
+                                    );
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor:
+                                        const Color.fromRGBO(50, 75, 80, 1),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(50),
+                                    ),
+                                    elevation: 0,
+                                    padding: (width <= 550)
+                                        ? const EdgeInsets.symmetric(
+                                            horizontal: 30, vertical: 20)
+                                        : const EdgeInsets.symmetric(
+                                            horizontal: 30, vertical: 25),
+                                    textStyle: TextStyle(
+                                        fontSize: (width <= 550) ? 13 : 17),
+                                  ),
+                                  child: const Text(
+                                    "NEXT",
+                                    style: TextStyle(
+                                      color: Color(0xffDCF6E6),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        )
-                      : Padding(
-                          padding: const EdgeInsets.all(30),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              TextButton(
-                                onPressed: () {
-                                  _controller.jumpToPage(2);
-                                },
-                                style: TextButton.styleFrom(
-                                  elevation: 0,
-                                  textStyle: TextStyle(
-                                    fontWeight: FontWeight.w600,
-                                    fontSize: (width <= 550) ? 13 : 17,
-                                  ),
-                                ),
-                                child: const Text(
-                                  "SKIP",
-                                  style: TextStyle(
-                                      color: Color.fromRGBO(50, 75, 80, 1)),
-                                ),
-                              ),
-                              ElevatedButton(
-                                onPressed: () {
-                                  _controller.nextPage(
-                                    duration: const Duration(milliseconds: 200),
-                                    curve: Curves.easeIn,
-                                  );
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor:
-                                      const Color.fromRGBO(50, 75, 80, 1),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(50),
-                                  ),
-                                  elevation: 0,
-                                  padding: (width <= 550)
-                                      ? const EdgeInsets.symmetric(
-                                          horizontal: 30, vertical: 20)
-                                      : const EdgeInsets.symmetric(
-                                          horizontal: 30, vertical: 25),
-                                  textStyle: TextStyle(
-                                      fontSize: (width <= 550) ? 13 : 17),
-                                ),
-                                child: const Text(
-                                  "NEXT",
-                                  style: TextStyle(
-                                    color: Color(0xffDCF6E6),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
